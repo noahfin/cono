a7b4acabcdfd7f3b60f431f8066a25e17ea062cf
Noah Finnerman <nonothetoad@gmail.com>
2016-03-02 18:55:27 -0500

    Add dashbord  page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,25 @@
 class CategoriesController < ApplicationController
+	def new
+	end
+	def index
+		@post =  Post.where("category_id = ?", params[:id])
+		
+		
+
+	end
+	def create
+
+	@category = Category.new(category_params)
+		if @category.save
+			redirect_to '/home'
+		else 
+			redirect_to 'login'
+		end
+	end
+	private
+		def category_params
+			params.require(:categories).permit(:name).merge(user_id: current_user.id)
+			
+		end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
 	def index
 
 	end
+	def home
+		@categories = Category.all
+
+	end
 end
